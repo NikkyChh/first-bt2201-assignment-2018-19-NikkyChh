a3ed06e0a7bbd80126bb365a3a5df84cf4d2f4e4
NikkyChh <ninikovska@uclan.ac.uk>
2019-02-01 11:58:28 +0000

    Week 11-PHP Variables, deadline: 01.02.2019.

---
+++ variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Variables-Week 11</title>
+</head>
+<body>
+<?php
+$name="Nikky";
+$major="Asia Pacific Studies";
+$country="Bulgaria";
+$thing="game";
+$game="Fire Emblem"; {
+    echo "Hello, my name is $name and I study $major.
+    I come from $country and my favourite $thing is $game.";
+}
+?>
+</body>
+</html>
